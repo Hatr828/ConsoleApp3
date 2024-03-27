30d136b0f46da4b06c4a0c272c11cbde1b5b33f8
Hatr828 <hatr626@gmail.com>
2024-03-27 10:18:25 -0500

    22

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,38 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
 
-public class SendMessage
+public class MyClass
 {
-    public delegate void MessageSender(string message);
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Value { get; set; }
+    public DateTime Date { get; set; }
+}
 
-    public MessageSender Sender { get; set; }
-
-    public void Message(string message)
+public static class Extensions
+{
+    public static int  CountSameNumber<T>(this IEnumerable<T> obj, T value)
     {
-        Sender?.Invoke(message);
+        return obj.Count(item => item.Equals(value));
     }
 }
 
 class Program
 {
-    static void MyMessage(string message)
-    {
-        Console.WriteLine($"gggggggg {message}");
-    }
-
     static void Main(string[] args)
     {
-        SendMessage sender = new SendMessage();
-
-        sender.Sender = MyMessage;
-
-        sender.Sender?.Invoke("some text");
-
-        sender.Sender = (message) =>
+        MyClass[] objects = new MyClass[]
         {
-            Console.WriteLine($"fffffff {message}");
+            new MyClass { Id = 1, Name = "Object 1", Description = "Description 1", Value = 10.5, Date = DateTime.Now },
+            new MyClass { Id = 2, Name = "Object 2", Description = "Description 2", Value = 20.3, Date = DateTime.Now },
+            new MyClass { Id = 3, Name = "Object 3", Description = "Description 3", Value = 30.8, Date = DateTime.Now }
         };
 
-        sender.Message("text2");
-        Console.ReadLine();
+        var sP = objects.Select(obj => new { obj.Name, obj.Value }).ToArray();
+        foreach (var obj in sP)
+        {
+            Console.WriteLine($"Name: {obj.Name}, Value: {obj.Value}");
+        }
+        ///////////////////////////////////////////////////////////////////////////
+        dynamic expando = new ExpandoObject();
+
+        expando.Name = "John";
+        expando.Age = 30;
+
+        Console.WriteLine($"Name: {expando.Name}, Age: {expando.Age}");
     }
+   
 }
